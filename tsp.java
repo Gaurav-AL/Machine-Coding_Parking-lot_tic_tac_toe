41218d81e62f7cdbf163e879ba453958197548ac
Gaurav-AL <reverse454@gmail.com>
2022-02-10 15:19:06 +0530

    InProgress

--- tsp.java
+++ tsp.java
@@ -1,11 +1,14 @@
+import java.lang.reflect.Array;
 import java.util.*;
 class City{
+    int index;
     double longitude,latitude;
     String name;
-    City(double x,double y, String name){
+    City(double x,double y, String name,int index){
         this.longitude = x;
         this.longitude = y;
         this.name = name;
+        this.index = index;
     }
 }
 class HillClimbing{
@@ -40,13 +43,28 @@ class Tsp{
         }
         return distances;
     }
+    public double calculateDistance(ArrayList<Integer> traverse){
+        double total_dist = 0.0;
+        for(int i = 0; i < traverse.size()-1;i++){
+            int index = traverse.get(i);
+            System.out.print(cities.get(index).name +"-->");
+            total_dist = euclidean_distance(cities.get(i).longitude,cities.get(i+1).latitude, cities.get(i).longitude,cities.get(i+1).longitude);    
+        }
+        System.out.print(total_dist);
+        System.out.println();
+        return total_dist;
+    }
+    public double minimizeDistance(){
+        double total_dost = Double.MAX_VALUE;
+        
+    }
     public static void main(String[] args) {
         ArrayList<City> cities = new ArrayList<>();
-        City c1 = new City(160.00,170.00,"Gurgaon");
-        City c2 = new City(180.00,1767.09,"Chambal");
-        City c3 = new City(196.60,180.890,"Fulgaon");
-        City c4 = new City(360.00,270.00,"Mahrajgang");
-        City c5 = new City(900.00,890.89,"Guwahati");
+        City c1 = new City(160.00,170.00,"Gurgaon",0);
+        City c2 = new City(180.00,1767.09,"Chambal",1);
+        City c3 = new City(196.60,180.890,"Fulgaon",2);
+        City c4 = new City(360.00,270.00,"Mahrajgang",3);
+        City c5 = new City(900.00,890.89,"Guwahati",4);
         cities.add(c1);
         cities.add(c2);
         cities.add(c3);
